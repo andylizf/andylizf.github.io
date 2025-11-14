78e51be85738ea988da6cabc669d6a7ca0dc60ec
Andy Lee <andylizf@outlook.com>
2025-11-14 03:32:45 +0000

    Simplify to basic Jekyll site with markdown only

--- vendor/bundle/ruby/3.3.0/gems/jekyll-archives-v2-0.0.6/lib/jekyll-archives-v2/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Jekyll
-  module ArchivesV2
-    VERSION = "0.0.6"
-  end
-end
--- vendor/bundle/ruby/3.3.0/gems/jekyll-scholar-7.2.2/lib/jekyll/scholar/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  class Scholar
-    VERSION = '7.2.2'.freeze
-  end
-end
